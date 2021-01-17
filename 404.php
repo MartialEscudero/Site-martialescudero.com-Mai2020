877a2bebf33429a4a5e5600aa9370bebde0548fd
MartialEscudero <martial.escudero@gmail.com>
2021-01-17 13:17:07 +0100

    Site VFinal Mai 2020 + Fix Contact Form

---
+++ 404.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="fr" prefix="og: http://ogp.me/ns#">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="white">
+    <title>404 !</title>
+    <link rel="icon" href="images/favicon.png" />
+    <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon.png" />
+  </head>
+
+  
+  <body>
+
+    <style>
+
+/* Source : https://codepen.io/conmarap/pen/mVMvVv */
+
+body {
+    background: #081421;
+    color: #d3d7de;
+    font-family: "Courier new";
+    font-size: 18px;
+    line-height: 1.5em;
+	cursor: default;
+}
+
+a {
+	color: #fff;
+}
+
+.code-area {
+    position: absolute;
+    width: 320px;
+	min-width: 320px;
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+}
+
+.code-area > span {
+    display: block;
+}
+
+@media screen and (max-width: 320px) {
+    .code-area {
+		font-size: 5vw;
+		min-width: auto;
+        width: 95%;
+		margin: auto;
+		padding: 5px;
+		padding-left: 10px;
+		line-height: 6.5vw;
+    }
+}
+
+
+    </style>
+
+<div class="code-area">
+  <span style="color: #777;font-style:italic;">
+    // 404 page not found.
+  </span>
+  <span>
+    <span style="color:#d65562;">
+      if
+    </span>
+	  (<span style="color:#4ca8ef;">!</span><span style="font-style: italic;color:#bdbdbd;">found</span>)
+    {
+  </span>
+  <span>
+    <span style="padding-left: 15px;color:#2796ec">
+       <i style="width: 10px;display:inline-block"></i>throw
+    </span>
+    <span>
+      (<span style="color: #a6a61f">"(╯°□°)╯︵ ┻━┻"</span>);
+    </span>
+	  <span style="display:block">}</span>
+	  <span style="color: #777;font-style:italic;">
+		  // <a href="https://www.martialescudero.com/">Go home!</a>
+	  </span>
+  </span>
+</div>
+
+  </body>
+  
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-164716890-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+  
+      gtag('config', 'UA-164716890-1');
+    </script>
+
+</html>
---
+++ README.md
@@ -0,0 +1,1 @@
+# site-martialescudero-Mai2020
